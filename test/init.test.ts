c800abf0fa7bf18f50b4339c518ef9abf54df6af
doyoungjeong <arktics@naver.com>
2022-06-24 17:47:45 +0900

    테스트 코드 수정

--- test/init.test.ts
+++ test/init.test.ts
@@ -1,7 +1,12 @@
 import "@testing-library/jest-dom/extend-expect";
 
+const fs = require("fs");
+const path = require("path");
+
+const value = fs.readFileSync(path.join(__dirname, "./source.txt")).toString();
+
 describe("initialTest", () => {
   it("Test!!! Pass", () => {
-    expect(1).toBe(1);
+    expect("1").toBe(value);
   });
 });
